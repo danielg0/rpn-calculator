b4a475e216cac7f045e020f1d5baad80cabf7d31
Daniel G <danielg.dev@gmail.com>
2019-03-29 15:05:35 +0000

    Setup UI
    Create a horizontal layout of vertical layouts holding buttons corresponding to numbers and operations, that, when pressed, add those symbols to a global statement variable.

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,101 @@
 
 #include "rpn.hpp"
 
-#include <string>
-
 #include <TGUI/TGUI.hpp>
 
+#include <iostream>
+
+#include <algorithm>
+#include <string>
+
+// Define global variable holding current rpn statement
+std::string STATEMENT = "";
+
+// Define functions for different button pushes
+void Enter(const sf::String& buttonText) {
+	auto ops = RPN::Operators();
+	// If an operator, push to statement surrounded by spaces
+	if(std::find(ops.begin(), ops.end(), buttonText) != ops.end()) {
+		 STATEMENT += " " + buttonText + " ";
+	}
+	// If an =, clear the statement
+	else if(buttonText == "=") {
+		STATEMENT = "";
+	}
+	else {
+		STATEMENT += buttonText;
+	}
+
+	// TODO: update text and perform calculation, if any
+	std::cout << STATEMENT << "\n";
+}
+
 int main() {
-	// Create render window
-	sf::RenderWindow window{{800, 600}, "RPN Calculator"};
-	tgui::Gui gui{window};
+	// Create render window and attach gui
+	sf::RenderWindow window {{300, 400}, "RPN Calculator",
+		sf::Style::Titlebar | sf::Style::Close};
+	tgui::Gui gui {window};
+
+	// Setup gui
+	// Create layout to hold buttons
+	auto layout = tgui::HorizontalLayout::create();
+	layout->setSize("100%", "70%");
+	layout->setPosition("0%", "30%");
+	gui.add(layout);
+
+	// Create rows of number buttons
+	for(int i = 2; i >= 0; --i) {
+		// Create vertical layout to hold buttons
+		auto numberButtons = tgui::VerticalLayout::create();
+		for(int j = 3; j <= 9; j += 3) {
+			// Create button for this number
+			auto button = tgui::Button::create(std::to_string(j - i));
+			button->connect("pressed", Enter);
+			numberButtons->add(button);
+		}
+
+		// Create a special button at the bottom
+		// . 0 =
+		// Decimal point
+		if(i == 2) {
+			auto button = tgui::Button::create(".");
+			button->connect("pressed", Enter);
+			numberButtons->add(button);
+		}
+		// Number 0
+		else if(i == 1) {
+			auto button = tgui::Button::create("0");
+			button->connect("pressed", Enter);
+			numberButtons->add(button);
+		}
+		// Equals sign (clears calculation)
+		else {
+			auto button = tgui::Button::create("=");
+			button->connect("pressed", Enter);
+			numberButtons->add(button);
+		}
+
+		// Add to root layout
+		layout->add(numberButtons);
+	}
+
+	// Create row of operator buttons
+	auto operatorButtons = tgui::VerticalLayout::create();
+	for(auto i : RPN::Operators()) {
+		auto button = tgui::Button::create(i);
+		button->connect("pressed", Enter);
+		operatorButtons->add(button);
+	}
+
+	// Add brackets buttons
+	for(auto i : {"(", ")"}) {
+		auto button = tgui::Button::create(i);
+		button->connect("pressed", Enter);
+		operatorButtons->add(button);
+	}
+
+	// Push all operator buttons to the root layout
+	layout->add(operatorButtons);
 
 	// Enter main window loop
 	while(window.isOpen()) {
